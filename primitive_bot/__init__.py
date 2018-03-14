81685f171bec9aa5ce6f38102bea2e17ba385269
ndklaps <nklapste@ualberta.ca>
2018-03-13 22:40:44 -0400

    removing useless BASEDIR generation

--- primitive_bot/__init__.py
+++ primitive_bot/__init__.py
@@ -7,12 +7,9 @@ A Discord bot that converts inputted images into primitive vector based
 graphics.
 
 Scripts:
-    __main__
+    - :mod:`__main__`
 
 Modules:
-    bot
+    - :mod:`bot`
+    - :mod:`images`
 """
-
-import os
-
-BASEDIR = os.path.dirname(os.path.realpath(__file__))
